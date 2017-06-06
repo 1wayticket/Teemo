e333c32b1e7bf01a3b3e205845c93ffc6a5b9779
gameplayer <cluchao@163.com>
2017-06-06 20:46:22 +0800

    first commit

---
+++ app/src/main/java/top/teemo/MainActivity.java
@@ -0,0 +1,16 @@
+package top.teemo;
+
+import android.os.BaseBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.teemo.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/top/teemo/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package top.teemo.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/6.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+}
