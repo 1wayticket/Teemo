3632ff214de6fc9a610770b4416040949f97ba7e
1wayticket <gameplayer2git>
2017-06-08 17:37:21 +0800

    添加jcvideoplayer

--- app/src/main/java/top/teemo/SplahActivity.java
+++ app/src/main/java/top/teemo/SplahActivity.java
@@ -1,8 +1,12 @@
 package top.teemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
+
+import java.util.concurrent.Delayed;
 
 import top.teemo.base.BaseActivity;
 
@@ -11,10 +15,17 @@ import top.teemo.base.BaseActivity;
  */
 
 public class SplahActivity extends BaseActivity {
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_splash);
+        enableSwipeBack(false);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        SystemClock.sleep(3000);
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
